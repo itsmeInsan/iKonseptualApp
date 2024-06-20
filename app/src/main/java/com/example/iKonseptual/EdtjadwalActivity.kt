545afac1901db28aa9533a79bb8945b8e001789e
Muhammmad Insan Faqqih Fiddin <minsanfaqqih@gmail.com>
2024-06-20 12:24:31 +0700

    feat: Edit Edit Jadwal Activity

--- app/src/main/java/com/example/iKonseptual/EdtjadwalActivity.kt
+++ app/src/main/java/com/example/iKonseptual/EdtjadwalActivity.kt
@@ -1,6 +1,8 @@
 package com.example.iKonseptual
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,7 +12,13 @@ import androidx.core.view.WindowInsetsCompat
 class EdtjadwalActivity : AppCompatActivity() {
 
     lateinit var label : TextView
-
+    private lateinit var editNama : EditText
+    private lateinit var editPerkara : EditText
+    private lateinit var editWaktu : EditText
+    private lateinit var editTempat : EditText
+    private lateinit var editJaksa : EditText
+    private lateinit var editKeperluan: EditText
+    private lateinit var btnEdit: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,6 +30,21 @@ class EdtjadwalActivity : AppCompatActivity() {
         }
 
         label = findViewById(R.id.label_edit)
+        editNama = findViewById(R.id.editNama)
+        editPerkara = findViewById(R.id.editPerkara)
+        editWaktu = findViewById(R.id.editWaktu)
+        editTempat = findViewById(R.id.editTempat)
+        editJaksa = findViewById(R.id.editJaksa)
+        editKeperluan = findViewById(R.id.editKeperluan)
+        btnEdit = findViewById(R.id.buttonedtJadwal)
         label.text = intent.getStringExtra("title_e")
+        btnEdit.setOnClickListener{
+            val nama = editNama.text.toString().trim()
+            val perkara = editPerkara.text.toString().trim()
+            val waktu = editWaktu.text.toString().trim()
+            val tempat = editTempat.text.toString().trim()
+            val jaksa = editJaksa.text.toString().trim()
+            val keperluan = editKeperluan.text.toString().trim()
+        }
     }
 }
