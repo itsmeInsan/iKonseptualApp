0936f6e7eb67c30b39dc2b118f51713d17c9b64f
Muhammmad Insan Faqqih Fiddin <minsanfaqqih@gmail.com>
2024-06-20 22:23:17 +0700

    feat: Create Get all Data Perkara penting

--- app/src/main/java/com/example/iKonseptual/UjppActivity.kt
+++ app/src/main/java/com/example/iKonseptual/UjppActivity.kt
@@ -2,15 +2,22 @@ package com.example.iKonseptual
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class UjppActivity : AppCompatActivity() {
 
     lateinit var det : CardView
+    lateinit var judul: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,9 +27,9 @@ class UjppActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
+        getAllDataPerkara()
         det = findViewById(R.id.CardView_det)
-
+        judul = findViewById(R.id.judulPerkaraTextView)
         det.setOnClickListener{
             val intent = Intent(
                 this,
@@ -30,6 +37,32 @@ class UjppActivity : AppCompatActivity() {
             )
             startActivity(intent)
         }
-
+    }
+    private fun getAllDataPerkara(){
+        PerkaraPentingClient.instance.getAll().enqueue(object : Callback<PerkaraPentingResponse> {
+            override fun onResponse(
+                call: Call<PerkaraPentingResponse>,
+                response: Response<PerkaraPentingResponse>
+            ) {
+                if(response.isSuccessful){
+                    val data = response.body()?.data
+                    if(data != null && data.isNotEmpty()){
+                        val firstItem = data[0]
+                        judul.text = firstItem.Judul_Perkara
+                        Toast.makeText(this@UjppActivity,"Sukses ambil data", Toast.LENGTH_SHORT).show()
+                    } else{
+                        judul.text = "N/A"
+                        Toast.makeText(this@UjppActivity,"Data tidak tersedia", Toast.LENGTH_SHORT).show()
+                    }
+                } else{
+                    val errorBody = response.errorBody()?.string() ?: "Galat"
+                    Toast.makeText(this@UjppActivity, "Gagal ambil data: ${response.code()} - $errorBody", Toast.LENGTH_SHORT).show()
+                }
+            }
+            override fun onFailure(call: Call<PerkaraPentingResponse>, t: Throwable) {
+                t.printStackTrace()
+                Toast.makeText(this@UjppActivity, "Failed get data: ${t.message}", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
