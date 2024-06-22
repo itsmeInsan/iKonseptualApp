2a57361de3fafb337ba5447352ae123504358550
Muhammmad Insan Faqqih Fiddin <minsanfaqqih@gmail.com>
2024-06-22 22:42:41 +0700

    feat: add intent get string extra on item

--- app/src/main/java/com/example/iKonseptual/AdppActivity.kt
+++ app/src/main/java/com/example/iKonseptual/AdppActivity.kt
@@ -11,30 +11,36 @@ import androidx.core.view.WindowInsetsCompat
 
 class AdppActivity : AppCompatActivity() {
 
-    lateinit var edit_pp : ImageView
+    lateinit var edit_pp: ImageView
     lateinit var textJudul: TextView
     lateinit var textTersangka: TextView
     lateinit var textPenahanan: TextView
     lateinit var textPasal: TextView
     lateinit var textkasus: TextView
     lateinit var btnDelete: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_adpp)
+
+        // Ensure window insets are set
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        edit_pp = findViewById(R.id.imageView_edit_pekating)
+        // Initialize views
+        edit_pp = findViewById(R.id.edit_pekating)
         textJudul = findViewById(R.id.text_Judul)
         textTersangka = findViewById(R.id.text_Tersangka)
         textPenahanan = findViewById(R.id.text_Penahanan)
         textPasal = findViewById(R.id.text_Pasal)
         textkasus = findViewById(R.id.text_Kasus_Posisi)
-        btnDelete = findViewById(R.id.imageView_delete_pekating)
+        btnDelete = findViewById(R.id.delete_pekating)
+
+
         val intent = intent
         val id = intent.getStringExtra("Id_Perkara")
         val judulPerkara = intent.getStringExtra("Judul_Perkara")
@@ -49,18 +55,18 @@ class AdppActivity : AppCompatActivity() {
         textPasal.text = pasal ?: "N/A"
         textkasus.text = kasusPosisi ?: "N/A"
 
-        edit_pp.setOnClickListener{
-             val nextSession = Intent(this@AdppActivity, AeppActivity::class.java).apply{
-                 putExtra("id",id)
-                 putExtra("judul",judulPerkara)
-                 putExtra("tersangka",identitasTersangka)
-                 putExtra("penahanan",penahanan)
-                 putExtra("pasal", pasal)
-                 putExtra("kasusPosisi", kasusPosisi)
-             }
+
+        edit_pp.setOnClickListener {
+            val nextSession = Intent(this@AdppActivity, AeppActivity::class.java).apply {
+                putExtra("id", id)
+                putExtra("judul", judulPerkara)
+                putExtra("tersangka", identitasTersangka)
+                putExtra("penahanan", penahanan)
+                putExtra("pasal", pasal)
+                putExtra("kasusPosisi", kasusPosisi)
+            }
             startActivity(nextSession)
             finish()
         }
     }
-
 }
